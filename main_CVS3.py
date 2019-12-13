77a12b6ebb0014781c1476c9cb6a53e2eb8ad658
coco <zj175@139.com>
2019-12-14 02:11:14 +0800

    增加实例

--- main_CVS3.py
+++ main_CVS3.py
@@ -1,15 +1,15 @@
 from CV import CV
 
 if  __name__ == '__main__':
-    d = r'D:\s\De\度盘\C#项目开发实战入门（光盘资源）\Video'
+    d = r'D:\s\De\度盘\VS2010轻松学习C#-从零到深入-天轰穿.NET4趣味编程视频教程'
     cv1 = CV(workDir=d, sleepTime=0)
     # speed = 1
     # speed = 1.1
     # speed = 1.2
-    # speed = 1.3
+    speed = 1.3
     # speed = 1.4
     # speed = 1.5
-    speed = 1.6
+    # speed = 1.6
     # speed = 1.8
     # speed = 2
     dealOldFilesMode = 0
